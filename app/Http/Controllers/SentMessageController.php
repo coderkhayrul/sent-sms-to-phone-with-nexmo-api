db462835a9827e04c3d0e987ed19ca6b70f4947b
coderkhayrul <coderkhayrul@gmail.com>
2022-11-10 11:45:13 +0600

    Sent SMS to Mobile Phone With Vonage

---
+++ app/Http/Controllers/SentMessageController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SentMessageController extends Controller
+{
+    public function sentMessage(Request $request)
+    {
+        // return $request->all();
+        $basic  = new \Vonage\Client\Credentials\Basic("54a526f4", "P83tAf2y8xJbth7O");
+        $client = new \Vonage\Client($basic);
+
+        $response = $client->sms()->send(
+            new \Vonage\SMS\Message\SMS($request->mobile_number, 'CoderKhayrul', $request->message)
+        );
+
+        $message = $response->current();
+
+        if ($message->getStatus() == 0) {
+            return redirect()->back()->with('success', "The message was sent successfully\n");
+        } else {
+            return redirect()->back()->with('errro', "The message failed with status: " . $message->getStatus() . "\n");
+        }
+    }
+}
